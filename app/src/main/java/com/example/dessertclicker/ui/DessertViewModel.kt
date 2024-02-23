7250e618db3e78bce8a2735b2a257ee826e31f87
Nicolas Castellano <nicolas.castellano.6@hotmail.com>
2024-02-22 20:24:56 -0400

    create ui state and view model

---
+++ app/src/main/java/com/example/dessertclicker/ui/DessertViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.dessertclicker.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.dessertclicker.data.DessertUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class DessertViewModel : ViewModel() {
+
+    // the ui states mutable and immutable
+    private val _dessertUiState = MutableStateFlow(DessertUiState())
+    val dessertUiState: StateFlow<DessertUiState> = _dessertUiState.asStateFlow()
+
+
+
+}
